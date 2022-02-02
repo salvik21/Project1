b59990bb3d64b9e3a5ff94e2dce6a050e660c391
salvik21 <86931898+salvik21@users.noreply.github.com>
2022-02-02 21:06:21 +0200

    added JS code to hamburger  menu and update html code

--- cart.js
+++ cart.js
@@ -1,8 +1,20 @@
 const navCart = document.getElementById("nav__cart");
 const cartMenu = document.getElementById("nav__cart-menu");
+// const navHamburger = document.getElementById("nav__hamburger");
+// const hamburgerMenu = document.getElementById("nav__hamburger-menu");
+// const hamburgerIcon = document.getElementById("nav__hamburger-hamburgerIcon");
+// const closeIcon = document.getElementById("nav__hamburger-closeIcon");
 
 navCart.addEventListener("click", toggleCart);
 
 function toggleCart() {
-  cartMenu.classList.toggle("nav__cart-menu--on");
+  if(hamburgerMenu.classList.contains("nav__hamburger-menu--on")) {
+    hamburgerMenu.classList.remove("nav__hamburger-menu--on");
+    hamburgerMenu.classList.add("nav__hamburger-menu--off");
+    closeIcon.classList.remove("nav__hamburger-menu--on");
+    closeIcon.classList.add("nav__hamburger-menu--off");
+    hamburgerIcon.classList.remove("nav__hamburger-menu--off");
+    hamburgerIcon.classList.add("nav__hamburger-menu--on");
+}
+cartMenu.classList.toggle("nav__cart-menu--on");
 }
