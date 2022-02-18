59ff1e84e0ee91aa1e0743ee5db0290c7884ffdc
salvik21 <86931898+salvik21@users.noreply.github.com>
2022-02-18 14:44:39 +0200

    added JS code to hamburger  menu and update html code

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,30 +1,22 @@
 const navHamburger = document.getElementById("nav__hamburger");
 const hamburgerMenu = document.getElementById("nav__hamburger-menu");
-const hamburgerIcon = document.getElementById("nav__hamburger-hamburgerIcon");
-const closeIcon = document.getElementById("nav__hamburger-closeIcon");
-// const navCart = document.getElementById("nav__cart");
-// const cartMenu = document.getElementById("nav__cart-menu");
+const hamburgerIcon = document.getElementById("nav__hamburger-icon");
+;
 
-navHamburger.addEventListener("click", toggleMenu);
+hamburgerIcon.addEventListener("click", toggleMenu);
 
 function toggleMenu () {
-if(cartMenu.classList.contains("nav__cart-menu--on")){
-  cartMenu.classList.toggle("nav__cart-menu--on");
-}
-if (hamburgerMenu.classList.contains("nav__hamburger-menu--on")) {
-    hamburgerMenu.classList.remove("nav__hamburger-menu--on");
-    hamburgerMenu.classList.add("nav__hamburger-menu--off");
-    closeIcon.classList.remove("nav__hamburger-menu--on");
-    closeIcon.classList.add("nav__hamburger-menu--off");
-    hamburgerIcon.classList.remove("nav__hamburger-menu--off");
-    hamburgerIcon.classList.add("nav__hamburger-menu--on");
+  if(!cartMenu.classList.contains("--off")){
+    cartMenu.classList.toggle("--off");
   }
+  if (!hamburgerMenu.classList.contains("--off")) {
+    hamburgerMenu.classList.toggle("--off");
+    hamburgerIcon.classList.toggle("change");
+    
+    }
   else {
-    hamburgerMenu.classList.add("nav__hamburger-menu--on");
-    hamburgerMenu.classList.remove("nav__hamburger-menu--off");
-    hamburgerIcon.classList.remove("nav__hamburger-menu--on");
-    hamburgerIcon.classList.add("nav__hamburger-menu--off");
-    closeIcon.classList.remove("nav__hamburger-menu--off");
-    closeIcon.classList.add("nav__hamburger-menu--on");
+    hamburgerMenu.classList.toggle("--off");
+    hamburgerIcon.classList.toggle("change");
+    
   }
 }
