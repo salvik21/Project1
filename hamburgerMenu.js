92fd5a9ab128832b9846a6194064cfd273c4601f
salvik21 <86931898+salvik21@users.noreply.github.com>
2022-02-18 21:08:37 +0200

    change code by you recommend

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,22 +1,19 @@
 const navHamburger = document.getElementById("nav__hamburger");
 const hamburgerMenu = document.getElementById("nav__hamburger-menu");
 const hamburgerIcon = document.getElementById("nav__hamburger-icon");
-;
 
 hamburgerIcon.addEventListener("click", toggleMenu);
 
 function toggleMenu () {
-  if(!cartMenu.classList.contains("--off")){
+  if(!cartMenu.classList.contains("--off")) {
     cartMenu.classList.toggle("--off");
   }
   if (!hamburgerMenu.classList.contains("--off")) {
     hamburgerMenu.classList.toggle("--off");
     hamburgerIcon.classList.toggle("change");
-    
     }
   else {
     hamburgerMenu.classList.toggle("--off");
     hamburgerIcon.classList.toggle("change");
-    
   }
 }
